5cc9c8b06bbdc1202540488c41b45375e403fd4d
Andrej <xentec@aix0.eu>
2019-12-27 11:26:28 +0100

    Migrate to tokio 0.2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
-
-
 use std::{
-	io, io::prelude::*,
-	net::TcpStream,
 	path::PathBuf,
+	str::FromStr,
 };
-
-use clap::{arg_enum};
-use structopt::{StructOpt};
-
-use log::{info, warn};
 
 use image::{Pixel, GenericImageView};
 use rand::{seq::SliceRandom};
 
-use r2d2;
+use clap::{arg_enum};
+use structopt::{StructOpt};
+
+use futures::{
+	future::FutureExt,
+	stream::StreamExt,
+	sink::SinkExt,
+};
+use tokio::{*,
+	io::AsyncWriteExt,
+};
+use tokio_util::codec::{Decoder};
+
+
+use log;
+
 
 arg_enum!{
-	#[derive(Debug,PartialEq)]
+	#[derive(Debug,Copy,Clone,PartialEq)]
 	enum Filter
 	{
 		Mask,
@@ -29,173 +36,194 @@ arg_enum!{
 #[derive(StructOpt, Debug)]
 struct Opt
 {
-	#[structopt(help = "The host to connect to")]
-	host: String,
-	#[structopt(short = "f", long = "filter", help = "filter to use", default_value="RGBA")]
-	filter: Filter,
-	#[structopt(long = "filter-color", help = "one-color", default_value="255")]
-	color: u8,
-	#[structopt(help = "Image to spray", parse(from_os_str))]
+	/// The host to connect to
+	#[structopt()]
+	host: std::net::SocketAddr,
+
+	/// Number of connections
+	#[structopt(short = "n", default_value = "8")]
+	num: usize,
+
+	/// Image to spray
+	#[structopt(parse(from_os_str))]
 	image: PathBuf,
-	#[structopt(long = "no-offset", help = "disable offset")]
+
+	/// Resize image
+	#[structopt(short = "r")]
+	resize: Option<String>,
+
+	/// Filter to use
+	#[structopt(short = "f", default_value="RGBA")]
+	filter: Filter,
+
+	/// Use a single color mask
+	#[structopt(long = "filter-color", default_value="255")]
+	color: u8,
+
+	/// Disable offset option
+	#[structopt(long = "no-offset")]
 	no_offset: bool,
 }
 
-fn main()
+
+fn main() -> Result<(), Box<dyn std::error::Error>>
 {
+	env_logger::Builder::from_default_env()
+		.format_timestamp_millis()
+		.filter_level(log::LevelFilter::Debug)
+		.init();
+
 	let opt = Opt::from_args();
+	log::info!("pixelspray: {:?}", &opt);
 
-	info!("pixelspray: {:?}", &opt);
+	runtime::Builder::new()
+		.threaded_scheduler()
+		.thread_name("rt-worker")
+		.thread_stack_size(80 * 1024) // musl libc stack size
+		.enable_all()
+		.build()?
+		.block_on(run(opt))
+}
 
-	let image = image::open(&opt.image).expect("failed to load image");
+async fn run(opt: Opt) -> Result<(), Box<dyn std::error::Error>>
+{
+	let mut image = image::open(&opt.image)?;
+
+	log::info!("connecting to {}...", opt.host);
+
+	let stream = net::TcpStream::connect(opt.host).await?;
+	let codec = tokio_util::codec::LinesCodec::new();
+	let mut stream = codec.framed(stream);
+
+	stream.send("SIZE".to_owned()).await?;
+	let res = stream.next().await
+	                .and_then(|res| res.ok());
+	let (sw,sh) = match res {
+		Some(s) => {
+			log::debug!("SIZE: {}", s);
+			let mut i = s.split_ascii_whitespace()
+						 .skip(1)
+			             .map(|s| u32::from_str(s).unwrap());
+
+			let w = i.next().unwrap();
+			let h = i.next().unwrap();
+			(w, h)
+		},
+		None => (1024, 768),
+	};
+	std::mem::drop(stream);
+
+	if let Some(resize) = opt.resize.as_ref() {
+		let mut i = resize.split('x').map(|s| u32::from_str(s).unwrap());
+		let w = i.next().unwrap();
+		let h = i.next().unwrap();
+		image = image.resize(w, h, image::imageops::FilterType::Lanczos3);
+	}
 
 	//image = image.resize(256, 256, image::FilterType::Nearest);
 	//image = image.grayscale();
 
-	let (sw,sh) = (1920, 1080);
-//	let (sw,sh) = (1024, 1024);
 	let (w,h) = image.dimensions();
 
 	let (xoff,yoff) = ((sw-w), sh-h);
-	let n = 8;
+	log::info!("screen: {}x{} image: {}x{} offset: {}x{}", sw, sh, w, h, xoff, yoff);
 
-	info!("screen: {}x{} image: {}x{} offset: {}x{}", sw, sh, w, h, xoff, yoff);
-
-	let mut pxs = image.pixels()
-		.filter_map(|(mut x,mut y,p)|
+	let mut pxls = image.pixels()
+		.filter(|pixel|
 		{
-			let ch = p.channels().len();
-			let (r,g,b,a) = p.channels4();
+			let (_x, _y, color) = pixel;
+			let (_r,_g,_b,a) = color.channels4();
+			a > 5
+		})
+		.map(|(mut x, mut y, color)| {
 
-			if a < 5 { return None; }
+			let (r,g,b,a) = color.channels4();
+			let ch = color.channels().len();
 
 			if opt.no_offset {
 				x += xoff;
 				y += yoff;
 			}
 
-			let px = match opt.filter
+			let mut filter = opt.filter;
+			if filter != Filter::Mask && r == g && g == b {
+				filter = Filter::Grey;
+			}
+
+			match filter
 			{
 				Filter::Mask => format!("PX {} {} {:02X}\n", x, y, opt.color),
 				Filter::Grey => format!("PX {} {} {:02X}\n", x, y, r),
 				Filter::RGBA if ch == 3 => format!("PX {} {} {:02X}{:02X}{:02X}\n", x, y, r, g, b),
 				Filter::RGBA => format!("PX {} {} {:02X}{:02X}{:02X}{:02X}\n", x, y, r, g, b, a),
-			};
-			Some(px)
-		}).collect::<Vec<_>>();
-
-	pxs.shuffle(&mut rand::thread_rng());
-
-	let mut bufs = Vec::new();
-	{
-		let mut buf = String::new();
-		for px in pxs
-		{
-			buf.push_str(&px);
-			if buf.len() > 1400
-			{
-				bufs.push(buf.clone());
-				buf.clear();
 			}
-		}
-		bufs.push(buf);
-	}
+		})
+		.collect::<Vec<_>>();
 
-	let c_size = bufs.len()/n+1;
+	pxls.shuffle(&mut rand::thread_rng());
 
-	info!("chunks: {} a {} pxs", bufs.len(), bufs.len()/n+1);
-	info!("Connecting to {}...", &opt.host);
+	let chunk_len = (pxls.len() + pxls.len() % opt.num) / opt.num;
 
-	let manager = DataStreamManager::new(opt.host,
-		if opt.no_offset { (0,0) } else { (xoff, yoff) });
-	let pool = r2d2::Pool::builder()
-		.max_size(15)
-		.build(manager)
-		.unwrap();
+	println!("Pixels: {}", pxls.len());
+	println!("Chunks: {} a {}", pxls.len() / chunk_len, chunk_len);
 
-	use std::thread;
+	let chunks = pxls.chunks(chunk_len)
+		.map(|chunk| chunk.concat())
+//		.map(Arc::new)
+		.collect::<Vec<_>>();
 
-	let mut threads = Vec::new();
-	for buf in bufs.chunks(c_size)
-	{
-		let pool = pool.clone();
-		let c = buf.clone();
-		let child = thread::spawn(move ||
-			loop {
-				let mut stream = pool.get().expect("no con");
-				for b in c {
-					stream.write_all(b.as_bytes()).ok();
+	let mut tasks = chunks.into_iter()
+		.map(|chunk| spawn(client(opt.host, chunk, (xoff, yoff))))
+		.collect::<futures::stream::FuturesUnordered<_>>();
+
+	loop {
+		futures::select! {
+			_ = signal::ctrl_c().fuse() => {
+				log::info!("stopping...");
+				break;
+			},
+			chunk = tasks.next() => {
+				if let Some(chunk) = chunk.and_then(|res| res.ok()) {
+					log::info!("respawning...");
+					tasks.push(spawn(client(opt.host, chunk, (xoff, yoff))));
+				} else {
+					break;
 				}
-			});
-		threads.push(child);
+			},
+		};
+	}
+
+	Ok(())
+}
+
+async fn client(host_addr: std::net::SocketAddr, chunk: String, offset: (u32, u32)) -> String {
+	let mut stream = match net::TcpStream::connect(host_addr).await {
+		Ok(v) => v,
+		Err(err) => {
+			log::error!("failed to connect: {}", err);
+			return chunk;
+		}
 	};
-
-	for t in threads
-	{
-		t.join().ok();
-	}
-}
-
-#[derive(Debug, Clone)]
-struct DataStreamManager
-{
-	host: String,
-	offset: (u32, u32),
-}
-
-impl DataStreamManager {
-	pub fn new(host: String, offset: (u32, u32)) -> Self
-	{
-		DataStreamManager { host: host, offset }
-	}
-}
-
-#[derive(Debug)]
-struct DataStream {
-	inner: TcpStream,
-	fail: Option<String>,
-}
-
-impl io::Write for DataStream {
-	fn write(&mut self, buf: &[u8]) -> io::Result<usize>
-	{
-		let res = self.inner.write(buf);
-		self.fail = res.as_ref().map_err(|e| e.to_string()).err();
-		res
+	log::info!("connected...");
+	if let Err(err) = stream.set_nodelay(true) {
+		log::warn!("failed to set no delay: {}", err);
 	}
 
-	fn flush(&mut self) -> io::Result<()>
-	{
-		let res = self.inner.flush();
-		self.fail = res.as_ref().map_err(|e| e.to_string()).err();
-		res
-	}
-}
-
-impl r2d2::ManageConnection for DataStreamManager {
-	/// The connection type this manager deals with.
-	type Connection = DataStream;
-
-	/// The error type returned by `Connection`s.
-	type Error = std::io::Error;
-
-	/// Attempts to create a new connection.
-	fn connect(&self) -> Result<Self::Connection, Self::Error>
-	{
-		info!("connecting... offset: {}x{}", self.offset.0, self.offset.1);
-		TcpStream::connect(&self.host)
-			.and_then(|s| s.set_nodelay(true).map(|_| s))
-			.and_then(|mut s| s.write_all(format!("OFFSET {} {}\n", self.offset.0, self.offset.1).as_bytes())
-				.map(|_| DataStream { inner: s, fail: None }))
+	let offset = format!("OFFSET {} {}\n", offset.0, offset.1);
+	if let Err(err) = stream.write_all(offset.as_bytes()).await {
+		log::error!("failed to set offset: {}", err);
+		return chunk;
 	}
 
-	fn is_valid(&self, _conn: &mut Self::Connection) -> Result<(), Self::Error>
-	{
-		Ok(())
+	let buf = chunk.as_bytes();
+	loop {
+		//log::debug!("sending {} bytes", buf.len());
+		if let Err(err) = stream.write_all(buf).await {
+			log::error!("failed to send: {}", err);
+			break;
+		}
+
 	}
-	fn has_broken(&self, conn: &mut Self::Connection) -> bool
-	{
-		if let Some(err) = &conn.fail { warn!("discarded: {}", err); }
-		conn.fail.is_some()
-	}
+
+	chunk
 }
